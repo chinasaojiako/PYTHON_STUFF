742198ad1bc5f460518df365d3f1401af533c32a
chinasaojiako <ojiako_chinasa@yahoo.com>
2022-03-26 16:49:03 +0000

    changed extra to excess

--- Ass3_exe3old.py
+++ Ass3_exe3old.py
@@ -13,7 +13,7 @@ luggage_diff = luggage_weight - Luggage_max
 
 if luggage_diff > 0 :
 	cost = (luggage_diff * 5 + (Luggage_max * 2))
-	print('The cost of your luggage is ' + str(cost) + ' because your luggage has an extra of ' + str(luggage_diff) + ' kg. Our company charges 5 per kg for excess weight over 50 kg.')
+	print('The cost of your luggage is ' + str(cost) + ' because your luggage has an excess of ' + str(luggage_diff) + ' kg. Our company charges 5 per kg for excess weight over 50 kg.')
 elif luggage_diff <= 0 :
 	cost = luggage_weight * 2
 	print('The cost of your luggage is ' + str(cost) + ' because your luggage was less than the maximum weight limit by ' + str(luggage_diff * -1) + ' kg. Our company charges 2 per kg for luggage weight less than 50 kg.')
